8605f8e2db299d8eab8c560f891faf8e27511236
RobertVinson <bbkkssv@gmail.com>
2026-09-08 08:57:12 -0500

    Initial Commit

---
+++ TrailMark/TrailMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMarkApp.swift
+//  TrailMark
+//
+//  Created by Robert Vinson on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
